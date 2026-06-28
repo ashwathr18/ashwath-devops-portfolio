d1cd0fe12ab341a83fbd018de9a7ef7d896e3ab6
Chandrakanth Avula <chandrakanthavula@Chandrakanths-MacBook-Air.local>
2026-06-28 17:40:34 +0530

    fixed config file

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,31 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
+const repoName = "ashwath-devops-portfolio";
+
 const nextConfig: NextConfig = {
-  devIndicators: false,
   output: "export",
   trailingSlash: true,
-  basePath: "/ashwath-devops-portfolio",
-  assetPrefix: "/ashwath-devops-portfolio",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   devIndicators: false,
+//   output: "export",
+//   trailingSlash: true,
+//   basePath: "/ashwath-devops-portfolio",
+//   assetPrefix: "/ashwath-devops-portfolio",
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
